c8a5aadec44e04627ccd05c9ba3f68806be845a9
Yayoi Ito <yayoi.t.ito@gmail.com>
2020-10-06 15:20:03 +0900

    ejs実装

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-ejs: ejs-lint
-	node_modules/gulp/bin/gulp.js ejs
+build: ejs-lint
+	node_modules/gulp/bin/gulp.js build
 
 ejs-lint:
 	find ejs | grep '.ejs' | xargs node_modules/ejs-lint/cli.js
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var rename = require("gulp-rename");
 var ejs = require("gulp-ejs");
 var replace = require("gulp-replace");
 var beautify = require("gulp-html-beautify");
+var exhibitData = require('./data/exhibits.json');
 
-gulp.task("ejs", (done) => {
+gulp.task("build", (done) => {
     gulp
         .src(["ejs/*.ejs", "!" + "ejs/_*.ejs"])
-        .pipe(ejs({}, {}, { ext: '.html' }))
+        .pipe(ejs({exhibitData: exhibitData}, {}, { ext: '.html' }))
         .pipe(rename({ extname: ".html" }))
         .pipe(beautify())
         .pipe(gulp.dest("./docs/"));
