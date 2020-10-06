d1253440ed9bb794b228fa08ae17b307257db557
Yayoi Ito <yayoi.t.ito@gmail.com>
2020-10-06 13:20:51 +0900

    biautifyも追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require("gulp");
 var rename = require("gulp-rename");
 var ejs = require("gulp-ejs");
 var replace = require("gulp-replace");
+var beautify = require("gulp-html-beautify");
 
 gulp.task("ejs", (done) => {
     gulp
         .src(["ejs/*.ejs", "!" + "ejs/_*.ejs"])
         .pipe(ejs({}, {}, { ext: '.html' }))
         .pipe(rename({ extname: ".html" }))
+        .pipe(beautify())
         .pipe(gulp.dest("./docs/"));
     done();
 });
