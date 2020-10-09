c5c19a4885337766c8d97e35e7006c277e24d03f
Yayoi Ito <yayoi.t.ito@gmail.com>
2020-10-09 09:25:05 +0900

    smooth-scroll

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-build: deploy-modaal ejs-lint
+build: ejs-lint
 	node_modules/gulp/bin/gulp.js build
 
 ejs-lint:
 	find ejs | grep '.ejs' | xargs node_modules/ejs-lint/cli.js
 
-deploy-modaal:
+npm-deploy:
 	cp node_modules/modaal/dist/js/modaal.min.js docs/js/.
+	cp node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js docs/js/.
 	cp node_modules/modaal/dist/css/modaal.min.css docs/css/.
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,17 +1,8 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+    easing: 'easeInOutQuad'
+});
 $(function () {
     // モーダルを出す
     $('.inline').modaal();
 
-    ///ページ内リンクをふわっと
-    $('.menu ul li a').click(function () {
-        // スクロールの速度
-        var speed = 700; // ミリ秒で記述
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $('body,html').animate({
-            scrollTop: position
-        }, speed, 'swing');
-        return false;
-    });
 });
